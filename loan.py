9e3605fb93da83f4405822368ef95cf24f9a8634
ravelindev <ravelindev@gmail.com>
2022-02-07 16:20:25 -0500

    Done with the programm final version

--- loan.py
+++ loan.py
@@ -1,3 +1,10 @@
+
+"""
+File: loan.py
+Author: Rudy Ravelin
+
+Purpose: Practice if statements with loan questions
+"""
 # This a program that will check if  a person is eligible for a loan.
 # It will ask the user for those information below and then check if they are eligible.
 # How large is the loan?
@@ -6,11 +13,9 @@
 # How large is your down payment?
 
 # The starting point of the program.
-print("Welcome to the Loan Eligibility Checker!")
-print(" ")
-print("This program will check if you are eligible for a loan.")
-print(" ")
-print("Enter a number from 1 to 10 on the following questions:")
+print("***Welcome to the Loan Eligibility Checker!***")
+print("***This program will check if you are eligible for a loan.***")
+print("***Enter a number from 1 to 10 on the following questions: ***")
 print(" ")
 
 # Now the questions listed below.
@@ -27,22 +32,38 @@ print("How large is your down payment?")
 down_payment = float(input("Enter your down payment: "))
 print(" ")
 # Now the conditions for the loan.
+should_loan = False # Ste the default value to false.
 if loan_amount >= 5:
     if credit_score >= 7 and income >= 7:
-        print("Yes, you are eligible for a loan.")
-    if (credit_score >= 7 or income >= 7) and down_payment >= 5:
-        print("Yes, you are eligible for a loan!")
-#         else:
-#             print("No, you are not eligible for a loan.")
-# if loan_amount < 5:
-#     if credit_score < 4:
-#         print("No, you are not eligible for a loan.")
-#     if credit_score >= 4:
-#         if income >= 7 or down_payment >= 7:
-#             print("Yes, you are eligible for a loan!")
-#         else:
-#             if income >= 4 and down_payment >= 4:
-#                 print("Yes, you are not eligible for a loan.")
-#             else:
-#                 print("No, you are not eligible for a loan.")
-# print(" ")
+        should_loan = True
+
+    if credit_score >= 7 or income >= 7:
+        if down_payment >= 5:
+             should_loan = True
+    else:
+        should_loan = False
+else:
+    if loan_amount < 4:
+        should_loan = False
+    if credit_score >=7 or down_payment >= 7:
+        should_loan = True
+    if credit_score >= 4 and down_payment  >= 4:
+        should_loan = True
+    else:
+        should_loan = False
+    print(" ")
+if should_loan:
+    print("You are eligible for a loan.")
+else:
+    print("You are not eligible for a loan.")
+print(" ")
+print("Thank you for using the Loan Eligibility Checker!")
+print(" ")
+print("Created by: Rudy Ravelin")
+print("Date: 02/Feb/2022")
+print("Version: 1.0")
+print("Copyright (c) 2022 Rudy Ravelin")
+print("All rights reserved.")
+print("This game is not for sale.")
+print("This game is not for resale.")
+print("Game created for educational purposes only.")
