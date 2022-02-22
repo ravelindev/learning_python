9b8fce66c43baad915d769d88b5c4d2bb9239ff4
ravelindev <ravelindev@gmail.com>
2022-02-22 17:16:49 -0500

    Practicing some for loop

---
+++ checkpoint_week08.py
@@ -0,0 +1,19 @@
+# This is to practice some of "FOR LOOP" concepts
+colors = ["red", "blue", "green", "yellow"]
+print()
+for color in colors:
+    print(f"This is the color: {color},capitalized: {color.capitalize()}")
+print()
+
+# This is to practice some of "FOR LOOP" concepts
+range(1, 9)
+print()
+for i in range(1, 9):
+    print(f"This is the number: {i}")
+print()
+
+# Even number from 2 to 20
+print()
+for i in range(2, 21, 2):
+    print(f"This is the number: {i}")
+print()
