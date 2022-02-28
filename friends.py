069f697d1c11b2770d98fc831e32d81aeb24ba46
ravelindev <ravelindev@gmail.com>
2022-02-28 18:17:45 -0500

    adding some comment to code for others

--- friends.py
+++ friends.py
@@ -1,19 +1,20 @@
 # This is an example of how to use list in python
-friends = []
+friends = [] # create an empty list
 
-new_friend = None
+new_friend = None # create a variable to hold a new friend
 
-while new_friend != "end":
-    new_friend = input("Type the name of a friend: ")    
-    if new_friend != "end":
-        friends.append(new_friend)
+while new_friend != "end": # loop until the user enters "end"
+    new_friend = input("Type the name of a friend: ") # ask the user for a new friend
+    if new_friend != "end": # if the user did not enter "end"
+        friends.append(new_friend) # add the new friend to the list
     
 
-print()
-print("Your friend are: ")
+print() # print a blank line
+print("Your friend are: ") # print a message
 
 
-for friend in friends:
-    print(friend)
+for friend in friends: # loop through the list
+    print(friend) # print each friend
 
-
+print() # print a blank line
+print("End of program") # print a message
