5d9146e95b645ff6236399100170ad742c57fd79
ravelindev <ravelindev@gmail.com>
2022-03-18 13:19:34 -0400

    Practice some list stuff

---
+++ list_favorite_numbers.py
@@ -0,0 +1,13 @@
+# this is to show my favprites numbers
+
+
+Fav_numbers  = [1, 3, 5, 7, 9, 11]
+
+print("My favorite numbers are:", Fav_numbers)
+print("The fisrt number is:", Fav_numbers[0])
+print("The last number is:", Fav_numbers[-1])
+print("The middle number is:", Fav_numbers[2])
+print("The second to last number is:", Fav_numbers[-2])
+print("The first three numbers are:", Fav_numbers[:3])
+print("The last three numbers are:", Fav_numbers[-3:])
+print("The first two numbers, and the last two numbers are:", Fav_numbers[:2] + Fav_numbers[-2:])
