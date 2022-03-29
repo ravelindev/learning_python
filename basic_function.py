0d7668964c5a3594157405e3ecc270b9eb064579
ravelindev <ravelindev@gmail.com>
2022-03-29 19:14:47 -0400

    Basic function practice

---
+++ basic_function.py
@@ -0,0 +1,10 @@
+ # This is to practice basic function in python
+
+ # To creat a function, we use def keyword
+def my_function():
+    print("Hello from a function")
+print("Hello from the main program")
+my_function()
+
+    # To call a function, we use the function name
+my_function()
