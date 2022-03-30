f4b2093fd95efd5bca650c6210839850d8b96ead
ravelindev <ravelindev@gmail.com>
2022-03-29 21:27:44 -0400

    Just practice more on basic function in python

---
+++ checkpoint_week13.py
@@ -0,0 +1,16 @@
+# This is some basci function in python
+
+def display_regular(message):
+    print(message)
+
+def display_upper(message):
+    print(message.upper())
+
+def display_lower(message):
+    print(message.lower())
+
+user_mesagwe = input("Enter a message: ")
+display_regular(user_mesagwe)
+display_upper(user_mesagwe)
+display_lower(user_mesagwe)
+
